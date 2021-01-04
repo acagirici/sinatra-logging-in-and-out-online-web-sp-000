cf277402bdb13f39d8e2a04ce2879b293a888250
Arif Cagirici <acagirici23@gmail.com>
2021-01-04 04:06:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-        erb :account
+    erb :account
 
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
