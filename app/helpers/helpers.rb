46292f50fe66ef47d4ec6495cd7f9aba8a0e4b83
Arif Cagirici <acagirici23@gmail.com>
2021-01-04 04:23:45 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
   
-  def self.current_user(session)
+  def self.current_user(session_hash)
     @user = User.find(session_hash[:user_id])
   end
 
-  def self.is_logged_in?(session)
+  def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
   end
   
